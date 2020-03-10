20edc25eee51cf5d45f1af88bbc1bc1164c392ee
Andre Skeene <halodre24@gmail.com>
2020-03-10 14:31:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,39 @@
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+  @@all = []
+  # @@count = 0
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    # save
+    @@all << self
+    # @@count += 1
+  end
+
+  # def save
+  #   @@all << self
+  # end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    # @@count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all
+  end
 end
